e0198c77d83d288552d7047cbca1cd5ace00bc64
Frigyes309 <frigyes.smuk@gmail.com>
2024-08-07 08:59:24 +0200

    Serialized

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,10 @@
-
 use lazy_static::lazy_static;
 use snow::{params::NoiseParams, HandshakeState};
-use std::cmp::PartialEq;
-
-#[cfg(any(feature = "server", feature = "client"))]
 use wasm_bindgen::prelude::*;
-#[cfg(any(feature = "server", feature = "client"))]
 use snow::Builder;
-#[cfg(any(feature = "server", feature = "client"))]
 use crate::NoiseHandshake::{SentEphemeralPublicKey, WaitingForConnection};
-#[cfg(any(feature = "server", feature = "client"))]
-use crate::Role::Initiator;
-#[cfg(any(feature = "server", feature = "client"))]
-use anyhow::Error;
-#[cfg(any(feature = "server", feature = "client"))]
 use NoiseHandshake::*;
-#[cfg(any(feature = "server", feature = "client"))]
-use tokio_tungstenite::tungstenite::protocol::Message;
-#[cfg(any(feature = "server", feature = "client"))]
+use std::fmt;
 type ResultHandler = Result<(NoiseHandshake, JsValue), anyhow::Error>;
 
 lazy_static! {
@@ -26,7 +13,6 @@ lazy_static! {
         .expect("Parsing a constant will cause no error");
 }
 
-#[allow(dead_code)]
 enum NoiseHandshake {
     WaitingForConnection,
     ReceivedPublicStaticKey,
@@ -36,31 +22,21 @@ enum NoiseHandshake {
     HandshakeCompleted,
 }
 
-#[allow(dead_code)]
-#[derive(PartialEq, Clone, Copy)]
-enum Role {
-    Initiator,
-    Responder,
-}
-
-#[allow(dead_code)]
-struct NoiseStateMachine<'a> {
-    role: Role,
+#[wasm_bindgen(js_name = NoiseStateMachine)]
+struct NoiseStateMachine {
+    role: bool, //true for client, false for server
     state: NoiseHandshake,
     handshaker: Handshaker,
-    up_func: &'a js_sys::Function,
-    down_func: &'a js_sys::Function,
+    up_func: js_sys::Function,
+    down_func: js_sys::Function,
 }
 
-#[cfg(feature = "client")]
 trait NoiseClient {
     fn initiator_start_handshake(&mut self) -> ResultHandler;
-    fn initiator_second_phase(&mut self, msg: Message) -> ResultHandler;
+    fn initiator_second_phase(&mut self, msg: String) -> ResultHandler;
     fn initiator_third_phase(&mut self) -> ResultHandler;
-
     fn initiator_send_request(&mut self) -> ResultHandler;
-
-    fn initiator_handle_response(&mut self, msg: Message) -> ResultHandler;
+    fn initiator_handle_response(&mut self, msg: String) -> ResultHandler;
 }
 
 struct Handshaker {
@@ -68,7 +44,6 @@ struct Handshaker {
     buf: Vec<u8>,
 }
 
-#[cfg(feature = "client")]
 impl NoiseClient for Handshaker {
     fn initiator_start_handshake(&mut self) -> ResultHandler {
         let len = self.noise.write_message(&[], &mut self.buf)?;
@@ -79,8 +54,8 @@ impl NoiseClient for Handshaker {
         Ok((SentEphemeralPublicKey, msg))
     }
 
-    fn initiator_second_phase(&mut self, msg: Message) -> ResultHandler {
-        self.noise.read_message(&msg.into_data(), &mut self.buf)?;
+    fn initiator_second_phase(&mut self, msg: String) -> ResultHandler {
+        self.noise.read_message(&msg.as_bytes(), &mut self.buf)?;
         let msg = match serde_wasm_bindgen::to_value("") {
             Ok(msg) => msg,
             Err(e) => todo!("Error: {:?}", e),
@@ -106,11 +81,11 @@ impl NoiseClient for Handshaker {
         Ok((HandshakeCompleted, msg))
     }
 
-    fn initiator_handle_response(&mut self, msg: Message) -> ResultHandler {
-        let start_len = self.buf.len();
-        let len = self.noise.read_message(&msg.into_data(), &mut self.buf)?;
+    fn initiator_handle_response(&mut self, msg: String) -> ResultHandler {
+        // let start_len = self.buf.len();
+        let len = self.noise.read_message(&msg.as_bytes(), &mut self.buf)?;
         let msg = &self.buf[..len];
-        print!("Startlen: {:?}\nLen: {:?}", start_len, len);
+        // print!("Startlen: {:?}\nLen: {:?}", start_len, len);
         let msg = match serde_wasm_bindgen::to_value(msg) {
             Ok(msg) => msg,
             Err(e) => todo!("Error: {:?}", e),
@@ -119,196 +94,108 @@ impl NoiseClient for Handshaker {
     }
 }
 
-// #[cfg(feature = "server")]
-/*trait NoiseServer {
-    fn responder_start_handshake(
-        noise: &mut HandshakeState,
-        msg: Message,
-        buf: &mut Vec<u8>,
-    ) -> ResultResponder;
-    fn responder_second_phase(noise: &mut HandshakeState, buf: &mut Vec<u8>) -> ResultSender;
-    fn responder_third_phase(
-        noise: &mut HandshakeState,
-        msg: Message,
-        buf: &mut Vec<u8>,
-    ) -> ResultResponder;
+#[wasm_bindgen(js_class = NoiseStateMachine)]
+impl NoiseStateMachine {
 
-    fn server_handle_connection(
-        noise: &mut HandshakeState,
-        msg: Message,
-        buf: &mut Vec<u8>,
-        state: NoiseHandshake,
-    ) -> ResultSender;
-
-    fn server_handle_request() -> ResultSender;
-
-    fn server_send_response() -> ResultSender;
-}
-
-#[cfg(feature = "server")]
-impl NoiseServer for HandshakeState {
-    fn responder_start_handshake(
-        noise: &mut HandshakeState,
-        msg: Message,
-        buf: &'static mut Vec<u8>,
-    ) -> ResultResponder {
-        noise.read_message(&msg.into_data(), buf)?;
-        Ok((ReceivedEphemeralPublicKey, buf))
-    }
-
-    fn responder_second_phase(
-        noise: &mut HandshakeState,
-        buf: &'static mut Vec<u8>,
-    ) -> ResultSender {
-        let len = noise.write_message(&[], buf)?;
-        Ok((SentStaticPublicKey, buf, len))
-    }
-
-    fn responder_third_phase(
-        noise: &mut HandshakeState,
-        msg: Message,
-        buf: &'static mut Vec<u8>,
-    ) -> ResultResponder {
-        noise.read_message(&msg.into_data(), buf)?;
-        Ok((WaitingForRequest, buf))
-    }
-
-    fn server_handle_connection(
-        noise: &mut HandshakeState,
-        msg: Message,
-        buf: &'static mut Vec<u8>,
-        state: NoiseHandshake,
-    ) -> ResultSender {
-        match state {
-            WaitingForConnection => match Self::responder_start_handshake(noise, msg, buf) {
-                Ok((state, buf)) => Ok((state, buf, 0)),
-                Err(e) => Err(e),
-            },
-            ReceivedEphemeralPublicKey => Self::responder_second_phase(noise, buf),
-            SentStaticPublicKey => match Self::responder_third_phase(noise, msg, buf) {
-                Ok((state, buf)) => Ok((state, buf, 0)),
-                Err(e) => Err(e),
-            },
-            WaitingForRequest => Self::server_handle_request(),
-            WaitingForResponse => Self::server_send_response(),
-            _ => Err(anyhow::anyhow!("Invalid state")),
-        }
-    }
-
-    fn server_handle_request() -> ResultSender {
-        Err(anyhow::anyhow!("Not implemented"))
-    }
-
-    fn server_send_response() -> ResultSender {
-        Err(anyhow::anyhow!("Not implemented"))
-    }
-}*/
-
-#[cfg(any(feature = "server", feature = "client"))]
-impl NoiseStateMachine<'_> {
-    #[allow(dead_code)] // Called from outside
-    fn new_init<'a>(
-        role: Role,
-        callback_up: &'a js_sys::Function,
-        callback_down: &'a js_sys::Function,
+    #[wasm_bindgen(constructor)]
+    pub fn new_init(
+        role: bool,
+        callback_up: js_sys::Function,
+        callback_down: js_sys::Function,
         server_static_key: Vec<u8>,
-    ) -> anyhow::Result<NoiseStateMachine<'a>, Error> {
+    ) -> Result<NoiseStateMachine, JsValue> {
         let builder = Builder::new(NOISE_PARAMS.clone());
-        let builder = builder.remote_public_key(&server_static_key)?;
-        let static_key = builder.generate_keypair()?.private;
+        let builder = builder.remote_public_key(&server_static_key).map_err(NoiseError::from)?;
+        let static_key = builder.generate_keypair().map_err(NoiseError::from)?.private;
         let secret: [u8; 32] = *b"Random 32 characters long secret";
         let noise: HandshakeState = builder
-            .local_private_key(&static_key)?
-            .psk(3, &secret.clone())?
-            .build_initiator()?;
+            .local_private_key(&static_key).map_err(NoiseError::from)?
+            .psk(3, &secret).map_err(NoiseError::from)?
+            .build_initiator().map_err(NoiseError::from)?;
         let mut buf: Vec<u8> = vec![0u8; 65535];
         let mut handshaker = Handshaker {
             noise,
             buf: buf.clone(),
         };
+
+        let state = if role {
+            let len = handshaker.noise.write_message(&[], &mut buf).map_err(NoiseError::from)?;
+            let msg = serde_wasm_bindgen::to_value(&handshaker.buf[..len])
+                .map_err(|e| JsValue::from_str(&format!("Serialization error: {:?}", e)))?;
+            callback_down.call1(&JsValue::NULL, &msg)?;
+            NoiseHandshake::SentEphemeralPublicKey
+        } else {
+            NoiseHandshake::WaitingForConnection
+        };
+
         Ok(NoiseStateMachine {
             role,
-            state: if Initiator == role {
-                // -> e, es
-                let len = handshaker.noise.write_message(&[], &mut buf)?;
-                let msg = match serde_wasm_bindgen::to_value(&handshaker.buf[..len]) {
-                    Ok(msg) => msg,
-                    Err(e) => todo!("Error: {:?}", e),
-                };
-                let _ = callback_down.call1(&JsValue::NULL, &msg);
-                SentEphemeralPublicKey
-            } else {
-                WaitingForConnection
-            },
+            state,
             handshaker,
             up_func: callback_up,
             down_func: callback_down,
         })
     }
-
+    
     // send mode is true if the message is to be sent to the server from the client
-    #[allow(dead_code)] // Called from outside
-    fn handle_connection(&mut self, msg: Option<Message>, send_mode: bool) -> () {
-        match self.role {
-            Role::Initiator =>
-            {
-                #[cfg(feature = "client")]
-                match self.state {
-                    WaitingForConnection => {
-                        let (new_state, msg) = match self.handshaker.initiator_start_handshake() {
+    #[wasm_bindgen(js_name = handleConnection)]
+    pub fn handle_connection(&mut self, msg: Option<String>, send_mode: bool) -> () {
+        if self.role {
+            match self.state {
+                WaitingForConnection => {
+                    let (new_state, msg) = match self.handshaker.initiator_start_handshake() {
+                        Ok((new_state, msg)) => (new_state, msg),
+                        Err(e) => todo!("Error: {:?}", e),
+                    };
+                    let _ = self.down_func.call1(&JsValue::NULL, &msg);
+                    self.state = new_state;
+                }
+                SentEphemeralPublicKey => {
+                    let msg = match msg {
+                        Some(msg) => msg,
+                        None => todo!("No message received"),
+                    };
+                    let (new_state, _msg) = match self.handshaker.initiator_second_phase(msg) {
+                        Ok((new_state, msg)) => (new_state, msg),
+                        Err(e) => todo!("Error: {:?}", e),
+                    };
+                    self.state = new_state;
+                    self.handle_connection(None, true);
+                }
+                ReceivedEphemeralPublicKey => {
+                    let (new_state, msg) = match self.handshaker.initiator_third_phase() {
+                        Ok((new_state, msg)) => (new_state, msg),
+                        Err(e) => todo!("Error: {:?}", e),
+                    };
+                    let _ = self.down_func.call1(&JsValue::NULL, &msg);
+                    self.state = new_state;
+                }
+
+                HandshakeCompleted => {
+                    if send_mode {
+                        let (new_state, msg) = match self.handshaker.initiator_send_request() {
                             Ok((new_state, msg)) => (new_state, msg),
                             Err(e) => todo!("Error: {:?}", e),
                         };
                         let _ = self.down_func.call1(&JsValue::NULL, &msg);
                         self.state = new_state;
-                    }
-                    SentEphemeralPublicKey => {
+                    } else {
                         let msg = match msg {
                             Some(msg) => msg,
                             None => todo!("No message received"),
                         };
-                        let (new_state, _msg) = match self.handshaker.initiator_second_phase(msg) {
-                            Ok((new_state, msg)) => (new_state, msg),
-                            Err(e) => todo!("Error: {:?}", e),
-                        };
-                        self.state = new_state;
-                        self.handle_connection(None, true);
-                    }
-                    ReceivedEphemeralPublicKey => {
-                        let (new_state, msg) = match self.handshaker.initiator_third_phase() {
-                            Ok((new_state, msg)) => (new_state, msg),
-                            Err(e) => todo!("Error: {:?}", e),
-                        };
-                        let _ = self.down_func.call1(&JsValue::NULL, &msg);
-                        self.state = new_state;
-                    }
-
-                    HandshakeCompleted => {
-                        if send_mode {
-                            let (new_state, msg) = match self.handshaker.initiator_send_request() {
+                        let (new_state, msg) =
+                            match self.handshaker.initiator_handle_response(msg) {
                                 Ok((new_state, msg)) => (new_state, msg),
                                 Err(e) => todo!("Error: {:?}", e),
                             };
-                            let _ = self.down_func.call1(&JsValue::NULL, &msg);
-                            self.state = new_state;
-                        } else {
-                            let msg = match msg {
-                                Some(msg) => msg,
-                                None => todo!("No message received"),
-                            };
-                            let (new_state, msg) =
-                                match self.handshaker.initiator_handle_response(msg) {
-                                    Ok((new_state, msg)) => (new_state, msg),
-                                    Err(e) => todo!("Error: {:?}", e),
-                                };
-                            let _ = self.up_func.call1(&JsValue::NULL, &msg);
-                            self.state = new_state;
-                        }
+                        let _ = self.up_func.call1(&JsValue::NULL, &msg);
+                        self.state = new_state;
                     }
-                    _ => todo!("Invalid state"),
                 }
+                _ => todo!("Invalid state"),
             }
-            Role::Responder => {
+        }else {
                 // #[cfg(feature = "server")]
                 /*match self.state {
                     WaitingForConnection => {
@@ -335,6 +222,41 @@ impl NoiseStateMachine<'_> {
                     WaitingForResponse => Self::server_send_response(),
                 }*/
             }
+    }
+}
+
+#[derive(Debug)]
+pub enum NoiseError {
+    SnowError(snow::Error),
+    SerdeError(serde_wasm_bindgen::Error),
+    OtherError(String),
+}
+
+impl fmt::Display for NoiseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            NoiseError::SnowError(e) => write!(f, "Snow error: {}", e),
+            NoiseError::SerdeError(e) => write!(f, "Snow error: {}", e),
+            NoiseError::OtherError(e) => write!(f, "Other error: {}", e),
         }
     }
 }
+
+impl std::error::Error for NoiseError {}
+
+impl From<snow::Error> for NoiseError {
+    fn from(error: snow::Error) -> NoiseError {
+        NoiseError::SnowError(error)
+    }
+}
+
+impl From<NoiseError> for JsValue {
+    fn from(error: NoiseError) -> JsValue {
+        JsValue::from_str(&error.to_string())
+    }
+}
+
+// pub enum Message {
+//     Text(String),
+//     Binary(Vec<u8>),
+// }
