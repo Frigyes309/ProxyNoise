d35c7b418f203ed5db992af02936be298a833935
Frigyes309 <frigyes.smuk@gmail.com>
2024-09-17 09:26:31 +0200

    Final state

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use serde_json::Value;
 use snow::{params::NoiseParams, Builder};
 use tokio::net::TcpListener;
 use tokio_tungstenite::{accept_async, connect_async, tungstenite::protocol::Message};
+use std::io::Write;
 
 pub type Result<T=(), E=anyhow::Error> = std::result::Result<T, E>;
 //pub type Error = Box<dyn std::error::Error>;
@@ -14,7 +15,7 @@ pub type Result<T=(), E=anyhow::Error> = std::result::Result<T, E>;
 const IP_PORT: &str = "127.0.0.1:3030";
 const PROXY_IP_PORT: &str = "127.0.0.1:9999";
 lazy_static! {
-    static ref NOISE_PARAMS: NoiseParams = "Noise_XXpsk3_25519_ChaChaPoly_BLAKE2s".parse().expect("Parsing a constant will cause no error"); //Might have to change to XK
+    static ref NOISE_PARAMS: NoiseParams = "Noise_XKpsk3_25519_ChaChaPoly_BLAKE2s".parse().expect("Parsing a constant will cause no error"); //Might have to change to XK
     static ref SECRET: [u8; 32] = *b"Random 32 characters long secret";
 }
 
@@ -22,8 +23,13 @@ async fn start_websocket_server() {
     let listener = TcpListener::bind(IP_PORT).await.expect("Failed to bind");
     println!("WebSocket server running on {}", IP_PORT);
 
+    let builder = Builder::new(NOISE_PARAMS.clone());
+    let static_key = builder.generate_keypair().unwrap();
+    println!("Public static key: {:?}", static_key.public.clone());
+
     while let Ok((stream, _)) = listener.accept().await {
-        tokio::spawn(handle_connection(stream));
+        println!("New connection detected...");
+        tokio::spawn(handle_connection(stream, static_key.private.clone()));
     }
 }
 
@@ -40,16 +46,16 @@ fn create_json(response: String, msg_id: &str) -> String {
     json
 }
 
-async fn handle_connection(stream: tokio::net::TcpStream) -> Result {
+async fn handle_connection(stream: tokio::net::TcpStream, static_key: Vec<u8>) -> Result {
     let ws_stream = accept_async(stream).await.context("Failed to accept WebSocket connection")?;
     let (mut write, mut read) = ws_stream.split();
 
     let builder = Builder::new(NOISE_PARAMS.clone());
-    let static_key = builder.generate_keypair()?.private;
     let mut noise = builder
         .local_private_key(&static_key)?
         .psk(3, &SECRET.clone())?
         .build_responder()?;
+
     let mut buf = vec![0u8; 65535];
 
     // <- e
@@ -78,13 +84,17 @@ async fn handle_connection(stream: tokio::net::TcpStream) -> Result {
     loop {
         match read.next().await {
             Some(Ok(msg)) => {
+                println!("Received a message...-{}-end of message", msg);
                 let len = noise.read_message(&msg.into_data(), &mut buf)?;
                 let msg = String::from_utf8_lossy(&buf[..len]);
+                println!("Client said: {}", msg);
                 let (response, msg_id) = local_server::run_client(PROXY_IP_PORT, msg).await?;
 
                 let response = create_json(response, &msg_id);
                 let len = noise.write_message(response.as_bytes(), &mut buf)?;
+                println!("Sending a message...");
                 write.send(Message::binary(&buf[..len])).await?;
+                println!("Sent a message...");
             }
             Some(Err(e)) => {
                 eprintln!("Websocket error: {:?}", e);
@@ -97,12 +107,13 @@ async fn handle_connection(stream: tokio::net::TcpStream) -> Result {
     Ok(())
 }
 
-async fn start_websocket_client() -> Result {
+async fn start_websocket_client(public_static_key: Vec<u8>) -> Result {
     let url = format!("ws://{}", IP_PORT);
     let (ws_stream, _response) = connect_async(&url).await.context("Failed to connect")?;
     let (mut write, mut read) = ws_stream.split();
 
     let builder = Builder::new(NOISE_PARAMS.clone());
+    let builder = builder.remote_public_key(&public_static_key)?;
     let static_key = builder.generate_keypair()?.private;
     let mut noise = builder
         .local_private_key(&static_key)?
@@ -209,7 +220,27 @@ async fn main() {
         println!("Proxy mo2de");
     } else if server_mode == 2 {
         println!("Client mode");
-        match start_websocket_client().await {
+        print!("Insert the public static key of the server: ");
+        // Read a Vec<u8> from stdin
+        let mut input = String::new();
+        print!("Enter comma-separated byte values (ex.: [1, 2, 3]): ");
+        std::io::stdout().flush().unwrap();  // Ensure the prompt is displayed
+
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        // Trim the input and remove the surrounding brackets
+        let trimmed = input.trim();
+        let without_brackets = &trimmed[1..trimmed.len()-1];
+
+        // Convert the input string to Vec<u8>
+        let public_static_key: Vec<u8> = without_brackets
+            .split(',')
+            .map(|x| x.trim().parse().expect("Invalid number"))
+            .collect();
+        println!("Public static key: {:?}", public_static_key);
+        match start_websocket_client(public_static_key).await {
             Err(e) => eprintln!("Error: {:?}", e),
             _ => (),
         }
